851cfed90a8aaf995834209fc7be13027e16f6c8
NKkrisz <nkkrisz2005@gmail.com>
2023-12-13 13:48:31 +0100

    cardMaker complete (hopefully)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.querySelectorAll("input").forEach((input) => {
     input.addEventListener("input", () => {
-        console.log(input.id, input.value, input.type);
+        console.log(input.id, input.value, input.type, input);
         
         if(input.type == "text"){
             document.querySelector(`#${input.id}-card`).innerText = input.value;
@@ -11,8 +11,22 @@ document.querySelectorAll("input").forEach((input) => {
         if(input.type == "color"){
             root.style.setProperty(`--${input.id}`, `${input.value}`);
         } else if(input.type == "range"){
-            root.style.setProperty(`--${input.id}`, `${input.value}px`);
+            if(input.id == "image-vertical" || input.id == "image-horizontal"){
+                root.style.setProperty(`--${input.id}`, `${input.value}%`);
+            } else {
+                root.style.setProperty(`--${input.id}`, `${input.value}px`);
+            }
         }
-    }
-)});
 
+        if(input.type == "file"){
+            if (input.files && input.files[0]) {
+                const reader = new FileReader();
+            
+                reader.onload = function (e) {
+                    document.querySelector("#photo-card").src = e.target.result;
+                };
+            
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+})});
