a6858184aaaac2da103999993ea3c5e89b3a54af
NKkrisz <nkkrisz2005@gmail.com>
2023-12-13 09:09:27 +0100

    Some progress in logic

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 document.querySelectorAll("input").forEach((input) => {
     input.addEventListener("input", () => {
-        console.log(input.id, input.value);
+        console.log(input.id, input.value, input.type);
+        
+        if(input.type == "text"){
+            document.querySelector(`#${input.id}-card`).innerText = input.value;
+        }
+
+        const root = document.querySelector(':root');
+
+        if(input.type == "color"){
+            root.style.setProperty(`--${input.id}`, `${input.value}`);
+        } else if(input.type == "range"){
+            root.style.setProperty(`--${input.id}`, `${input.value}px`);
+        }
     }
 )});
+
